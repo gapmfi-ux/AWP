4e3a399ccf0bfb01c414fa11b5f940a63e4ed5d4
gapmfi-ux <gapmfi@gmail.com>
2026-04-08 14:00:58 +0000

    Update pv.js

--- assets/js/pv.js
+++ assets/js/pv.js
@@ -1,7 +1,3 @@
-/* ============================================
-   PAYMENT VOUCHER MODULE JAVASCRIPT
-   Maintains original Google Apps Script logic
-   ============================================ */
 
 // Payment Voucher Module JavaScript
 let lastSubmittedVoucherData = null;
