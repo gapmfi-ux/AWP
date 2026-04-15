6481f93f500222db7c99e370a00167b83500de3b
gapmfi-ux <gapmfi@gmail.com>
2026-04-15 17:28:30 +0000

    Add subscription-related API methods

--- assets/js/api.js
+++ assets/js/api.js
@@ -220,6 +220,60 @@ class ApiService {
   async getAllInvestments(options = {}) {
     return this.request('getAllInvestments', {}, options);
   }
+
+  // ============================================
+// SUBSCRIPTION API
+// ============================================
+
+async getSubscriptionCategories(options = {}) {
+  this.log('Getting subscription categories');
+  return this.request('getSubscriptionCategories', {}, options);
+}
+
+async generateSubscriptionCategoryCode(options = {}) {
+  this.log('Generating subscription category code');
+  return this.request('generateSubscriptionCategoryCode', {}, options);
+}
+
+async getNextSubscriptionCode(categoryCode, options = {}) {
+  this.log('Getting next subscription code for:', categoryCode);
+  return this.request('getNextSubscriptionCode', { categoryCode }, options);
+}
+
+async addSubscription(formData, options = {}) {
+  this.log('Adding subscription:', formData);
+  return this.request('addSubscription', { formData: JSON.stringify(formData) }, options);
+}
+
+async getAllSubscriptions(options = {}) {
+  this.log('Getting all subscriptions');
+  return this.request('getAllSubscriptions', {}, options);
+}
+
+async updateSubscription(formData, options = {}) {
+  this.log('Updating subscription:', formData);
+  return this.request('updateSubscription', { formData: JSON.stringify(formData) }, options);
+}
+
+async deleteSubscription(subscriptionId, options = {}) {
+  this.log('Deleting subscription:', subscriptionId);
+  return this.request('deleteSubscription', { subscriptionId }, options);
+}
+
+async getSubscriptionsByDateRange(fromDate, toDate, options = {}) {
+  this.log('Getting subscriptions by date range:', fromDate, toDate);
+  return this.request('getSubscriptionsByDateRange', { fromDate, toDate }, options);
+}
+
+async getExpiredSubscriptions(asOfDate, options = {}) {
+  this.log('Getting expired subscriptions as of:', asOfDate);
+  return this.request('getExpiredSubscriptions', { asOfDate }, options);
+}
+
+async renewSubscription(subscriptionId, newExpiryDate, newAnnualCost, options = {}) {
+  this.log('Renewing subscription:', subscriptionId);
+  return this.request('renewSubscription', { subscriptionId, newExpiryDate, newAnnualCost }, options);
+}
   
   // ============================================
   // TEST CONNECTION
